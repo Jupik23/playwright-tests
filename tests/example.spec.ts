17bfeeb52ec39245e774ca242fc739caf89eec80
Jupik23 <jupekkekw@gmail.com>
2026-01-15 08:24:35 +0100

    first test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,9 +3,13 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://demo-bank.vercel.app/');
   await page.getByTestId('login-input').click();
+  await page.getByTestId('login-input').fill('te');
+  await page.getByTestId('login-input').click();
   await page.getByTestId('login-input').fill('test1234');
-  await page.getByTestId('password-input').dblclick();
+  await page.getByTestId('password-input').click();
   await page.getByTestId('password-input').fill('test1234');
   await page.getByTestId('login-button').click();
-  await page.locator('div').nth(3).click();
+  await page.getByTestId('user-name').dblclick();
+
+  await expect(page.getByTestId('user-name')).toHaveText('Jan Demobankowy');
 });
